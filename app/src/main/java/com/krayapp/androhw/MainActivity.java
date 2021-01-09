b2702bc7b35220756bb09a27cc7da07daf1ed477
krayman <50882897+krayman@users.noreply.github.com>
2021-01-09 19:07:52 +0700

    Задание 1

--- app/src/main/java/com/krayapp/androhw/MainActivity.java
+++ app/src/main/java/com/krayapp/androhw/MainActivity.java
@@ -3,6 +3,8 @@ package com.krayapp.androhw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button prettyButton = findViewById(R.id.pretty_button);
+        prettyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prettyButton.setText("Or not :(");
+            }
+        });
     }
 }
