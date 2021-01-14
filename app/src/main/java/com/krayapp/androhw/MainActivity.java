6af3d8f2014a840a0eee4ebba7c10eb443dd5ac0
krayman <50882897+krayman@users.noreply.github.com>
2021-01-14 21:29:04 +0700

    Допилил шрифты

--- app/src/main/java/com/krayapp/androhw/MainActivity.java
+++ app/src/main/java/com/krayapp/androhw/MainActivity.java
@@ -1,16 +1,27 @@
 package com.krayapp.androhw;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button1:
+
+        }
+    }
+
 }
